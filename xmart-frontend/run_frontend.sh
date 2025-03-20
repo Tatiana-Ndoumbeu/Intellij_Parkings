751957b833b990e00722b16d2edf79908daace93
Junior Fokou <juniorfokou995@yahoo.fr>
2025-03-20 17:04:20 +0100

    script lancement front

---
+++ xmart-frontend/run_frontend.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LOCAL_FRONT="/Users/user/Desktop/Travail/Sirius/Intellij_Parkings/"         # chemin vers le projet (chacun remplace)
+
+LOCAL_JAR="/Users/user/Desktop/Travail/Sirius/Intellij_Parkings/xmart-frontend/target/xmart-frontend-1.0-SNAPSHOT-jar-with-dependencies.jar" #chemin du .jar créé (chacun remplace aussi)
+
+echo "Compilation du PROJET"
+mvn package "$LOCAL_FRONT"
+
+echo "Démarrage du frontend"
+java -jar "$LOCAL_JAR"
