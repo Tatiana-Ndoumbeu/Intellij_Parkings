c341ec9195dcb5e6c02d93793630e20939d74abe
Junior Fokou <juniorfokou995@yahoo.fr>
2025-03-21 14:06:09 +0100

    LOGO principal

--- xmart-frontend/run_frontend.sh
+++ xmart-frontend/run_frontend.sh
@@ -5,7 +5,9 @@ LOCAL_FRONT="/Users/user/Desktop/Travail/Sirius/Intellij_Parkings/"         # ch
 LOCAL_JAR="/Users/user/Desktop/Travail/Sirius/Intellij_Parkings/xmart-frontend/target/xmart-frontend-1.0-SNAPSHOT-jar-with-dependencies.jar" #chemin du .jar créé (chacun remplace aussi)
 
 echo "Compilation du PROJET"
-mvn package "$LOCAL_FRONT"
+#mvn package "$LOCAL_FRONT"
+cd "$LOCAL_FRONT"
+mvn package
 
 echo "Démarrage du frontend"
 java -jar "$LOCAL_JAR"
