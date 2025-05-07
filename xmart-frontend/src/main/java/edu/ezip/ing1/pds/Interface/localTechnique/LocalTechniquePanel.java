53515900956002ea7343fee22ffb46197fe821f4
Tatiana-Ndoumbeu <tatianandoumbeu@yahoo.com>
2025-05-07 13:37:00 +0200

    refactorization du code pour faire une archi propre

--- xmart-frontend/src/main/java/edu/ezip/ing1/pds/Interface/localTechnique/LocalTechniquePanel.java
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/Interface/localTechnique/LocalTechniquePanel.java
@@ -17,7 +17,7 @@ import java.awt.event.*;
 import java.io.IOException;
 import java.util.List;
 
-import static edu.ezip.ing1.pds.Formulaires.FormulairePaiementTechnique;
+
 import static edu.ezip.ing1.pds.Formulaires.chargerIcone;
 
 public class LocalTechniquePanel extends JPanel {
